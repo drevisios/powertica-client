3bc7c9559e9f11a92f363c9ab991ba95280d20f1
dionysis <d.revisios@gmail.com>
2024-09-23 08:51:30 +0300

    download dataset

--- powertica_client/client.py
+++ powertica_client/client.py
@@ -1,7 +1,37 @@
+import os
+from typing import Optional
+import azure.ai.ml._artifacts._artifact_utilities as artifact_utils
+from azure.identity import InteractiveBrowserCredential
+from azure.ai.ml import MLClient
+import uuid
+import hashlib
+import pandas as pd
+
 
 class PowerticaClient:
-    def __init__(self):
-        pass
+    def __init__(self, tenant_id: str):
+        self.tenant_id = tenant_id
+        self.cred = InteractiveBrowserCredential(tenant_id=self.tenant_id)
+        self.client = MLClient.from_config(credential=self.cred)
 
-    def hello_world(self):
-        print('Hello World! 2')
+    def _download_artifact(self, uri: str, cache_key: Optional[str] = None) -> str:
+        cache_key = cache_key or uuid.uuid4().hex
+        destination = f".artifacts/{cache_key}"
+
+        if os.path.exists(destination):
+            return destination
+        else:
+            os.makedirs(destination)
+
+        artifact_utils.download_artifact_from_aml_uri(uri,
+                                                      destination=destination,
+                                                      datastore_operation=self.client.datastores
+                                                      )
+        return destination
+
+    def download_dataset(self, name: str, version: str, pattern: str) -> pd.DataFrame:
+        dataset = self.client.data.get(name=name, version=version)
+        uri = dataset.path + pattern
+        cache_key = hashlib.sha256(uri.encode('utf-8')).hexdigest()
+        dir = self._download_artifact(uri=uri, cache_key=cache_key)
+        return pd.read_parquet(dir)
--- setup.py
+++ setup.py
@@ -5,6 +5,12 @@ setup(
     version='0.1.1',
     packages=find_packages(),
     description='A client for Powertica tools',
+    install_requires=[
+        'azure-ai-ml',
+        'azure-identity',
+        'pandas',
+        'pyarrow'
+    ],
     author='Dionysis Revisios',
     author_email='d.revisios@gmail.com',
     url='https://github.com/yourusername/my_private_package',
