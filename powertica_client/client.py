8d5294cae23506cc2b05cd50db97d322671deb73
dionysis <d.revisios@gmail.com>
2024-09-23 09:11:29 +0300

    download model

--- powertica_client/client.py
+++ powertica_client/client.py
@@ -1,17 +1,20 @@
 import os
+from linecache import cache
 from typing import Optional
 import azure.ai.ml._artifacts._artifact_utilities as artifact_utils
-from azure.identity import InteractiveBrowserCredential
+from azure.identity import InteractiveBrowserCredential, AzureCliCredential
 from azure.ai.ml import MLClient
 import uuid
 import hashlib
+from sklearn.pipeline import Pipeline
 import pandas as pd
+import cloudpickle
 
 
 class PowerticaClient:
-    def __init__(self, tenant_id: str):
+    def __init__(self, tenant_id: str, use_cli_creds:bool=False):
         self.tenant_id = tenant_id
-        self.cred = InteractiveBrowserCredential(tenant_id=self.tenant_id)
+        self.cred = AzureCliCredential() if use_cli_creds else InteractiveBrowserCredential(tenant_id=self.tenant_id)
         self.client = MLClient.from_config(credential=self.cred)
 
     def _download_artifact(self, uri: str, cache_key: Optional[str] = None) -> str:
@@ -33,5 +36,14 @@ class PowerticaClient:
         dataset = self.client.data.get(name=name, version=version)
         uri = dataset.path + pattern
         cache_key = hashlib.sha256(uri.encode('utf-8')).hexdigest()
-        dir = self._download_artifact(uri=uri, cache_key=cache_key)
-        return pd.read_parquet(dir)
+        files_dir = self._download_artifact(uri=uri, cache_key=cache_key)
+        return pd.read_parquet(files_dir)
+
+    def download_model(self, name: str, version: str) -> Pipeline:
+        model = self.client.models.get(name=name, version=version)
+        uri = model.path
+        cache_key = hashlib.sha256(uri.encode('utf-8')).hexdigest()
+        files_dir = self._download_artifact(uri=uri, cache_key=cache_key)
+        first_file_path = os.path.join(files_dir, os.listdir(files_dir)[0])
+        pipeline = cloudpickle.load(open(first_file_path, "rb"))
+        return pipeline
--- setup.py
+++ setup.py
@@ -2,14 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name='powertica_client',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
     description='A client for Powertica tools',
     install_requires=[
         'azure-ai-ml',
         'azure-identity',
         'pandas',
-        'pyarrow'
+        'pyarrow',
+        'scikit-learn',
+        'cloudpickle',
+        'scikeras',
+        'tensorflow'
     ],
     author='Dionysis Revisios',
     author_email='d.revisios@gmail.com',
