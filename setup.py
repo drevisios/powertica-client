575bd38567cd7f867e129bc825cfb3f5258fd469
dionysis <d.revisios@gmail.com>
2024-09-23 08:51:51 +0300

    bump version 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='powertica_client',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     description='A client for Powertica tools',
     install_requires=[
