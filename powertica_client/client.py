5cdbf3f022d009b3b940191acd80bc4ad752f702
dionysis <d.revisios@gmail.com>
2024-09-23 08:19:30 +0300

    first commit

---
+++ powertica_client/client.py
@@ -0,0 +1,7 @@
+
+class PowerticaClient:
+    def __init__(self):
+        pass
+
+    def hello_world(self):
+        print('Hello World!')
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='powertica_client',
+    version='0.1.0',
+    packages=find_packages(),
+    description='A client for Powertica tools',
+    author='Dionysis Revisios',
+    author_email='d.revisios@gmail.com',
+    url='https://github.com/yourusername/my_private_package',
+)
